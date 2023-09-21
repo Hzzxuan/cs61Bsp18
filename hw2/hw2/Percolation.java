ec5bfae237bed219b8259cc5d288b0c40d08eb08
Hzzxuan <hzxuan1225@gmail.com>
2023-09-21 22:44:16 +0800

    update hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,116 @@
 package hw2;
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+import java.util.ArrayList;
 
+public class Percolation{
+    private boolean[][] grid;
+    private int size;
+    private int vertical;
+    private WeightedQuickUnionUF union1;
+    //row*vertical+col
+    public Percolation(int N){
+        grid = new boolean[N][N];
+        for(int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                grid[i][j]= false;
+            }
+        }
+        size = 0;
+        vertical = N;
+        union1 = new WeightedQuickUnionUF(N*N+1);
+
+    }                // create N-by-N grid, with all sites initially blocked
+    public void open(int row, int col){
+        if(row>=vertical ||col>=vertical ||row <0||col<0){
+            throw new IllegalArgumentException("Argument Out of range");
+        }
+        grid[row][col] = true;
+        size += 1;
+        if(row==0 && col==0){
+            if(isOpen(row,col+1)){union1.union(UnionIndex(row, col),UnionIndex(row,col+1));}
+            if(isOpen(row+1,col)){union1.union(UnionIndex(row, col),UnionIndex(row+1,col));}
+        } else if (row==0 && col==vertical-1) {
+            if(isOpen(row,col-1)){union1.union(UnionIndex(row, col),UnionIndex(row,col-1));}
+            if(isOpen(row+1,col)){union1.union(UnionIndex(row, col),UnionIndex(row+1,col));}
+        } else if (row==vertical-1 && col==0) {
+            if(isOpen(row,col+1)){union1.union(UnionIndex(row, col),UnionIndex(row,col+1));}
+            if(isOpen(row-1,col)){union1.union(UnionIndex(row, col),UnionIndex(row-1,col));}
+        } else if (row==vertical-1 && col==vertical-1) {
+            if(isOpen(row,col-1)){union1.union(UnionIndex(row, col),UnionIndex(row,col-1));}
+            if(isOpen(row-1,col)){union1.union(UnionIndex(row, col),UnionIndex(row-1,col));}
+        } else if (row==0) {
+            if(isOpen(row,col-1)){union1.union(UnionIndex(row, col),UnionIndex(row,col-1));}
+            if(isOpen(row,col+1)){union1.union(UnionIndex(row, col),UnionIndex(row,col+1));}
+            if(isOpen(row+1,col)){union1.union(UnionIndex(row, col),UnionIndex(row+1,col));}
+        } else if (row==vertical-1) {
+            if(isOpen(row,col-1)){union1.union(UnionIndex(row, col),UnionIndex(row,col-1));}
+            if(isOpen(row,col+1)){union1.union(UnionIndex(row, col),UnionIndex(row,col+1));}
+            if(isOpen(row-1,col)){union1.union(UnionIndex(row, col),UnionIndex(row-1,col));}
+        } else if (col==0) {
+            if(isOpen(row,col+1)){union1.union(UnionIndex(row, col),UnionIndex(row,col+1));}
+            if(isOpen(row-1,col)){union1.union(UnionIndex(row, col),UnionIndex(row-1,col));}
+            if(isOpen(row+1,col)){union1.union(UnionIndex(row, col),UnionIndex(row+1,col));}
+        } else if (col==vertical-1) {
+            if(isOpen(row,col-1)){union1.union(UnionIndex(row, col),UnionIndex(row,col-1));}
+            if(isOpen(row-1,col)){union1.union(UnionIndex(row, col),UnionIndex(row-1,col));}
+            if(isOpen(row+1,col)){union1.union(UnionIndex(row, col),UnionIndex(row+1,col));}
+        }else {
+            if(isOpen(row,col+1)){union1.union(UnionIndex(row, col),UnionIndex(row,col+1));}
+            if(isOpen(row,col-1)){union1.union(UnionIndex(row, col),UnionIndex(row,col-1));}
+            if(isOpen(row-1,col)){union1.union(UnionIndex(row, col),UnionIndex(row-1,col));}
+            if(isOpen(row+1,col)){union1.union(UnionIndex(row, col),UnionIndex(row+1,col));}
+        }
+
+    }       // open the site (row, col) if it is not open already
+    private int UnionIndex(int row ,int col){
+        return row*vertical+col;
+    }
+
+    public boolean isOpen(int row, int col){
+        if(row>=vertical ||col>=vertical ||row <0||col<0){
+            throw new IllegalArgumentException("Argument Out of range");
+        }
+        return grid[row][col];
+    }  // is the site (row, col) open?
+    public boolean isFull(int row, int col){
+        if(row>=vertical ||col>=vertical ||row <0||col<0){
+            throw new IllegalArgumentException("Argument Out of range");
+        }
+        if(UnionIndex(row,col)>=0&&UnionIndex(row,col)<vertical-1){
+            return true;
+        }
+        else {
+            for(int i=0;i<vertical-1;i++){
+                if(union1.connected(UnionIndex(row,col),i)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }  // is the site (row, col) full?
+    public int numberOfOpenSites(){
+        return size;
+    }           // number of open sites
+    public boolean percolates() {
+        for(int j=0;j<vertical;j++){
+            for(int i=0;i<vertical;i++){
+                if(union1.connected(UnionIndex(vertical-1,j),UnionIndex(0,i))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }             // does the system percolate?
+    public static void main(String[] args){
+        Percolation x =new Percolation(4);
+        x.open(0,0);
+        x.open(1,0);
+        x.open(2,0);
+        boolean y = x.percolates();
+        System.out.println(y);
+        x.open(3,0);
+        boolean z = x.percolates();
+        System.out.println(z);
+    }   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,39 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
+    int TotalTime;
+    int [] ArrayX;
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        TotalTime =T ;
+        ArrayX = new int[N];
+        for(int i =0 ;i<TotalTime;i++){
+            Percolation PercolationSample=pf.make(N);
+            int x = 0;
+            while(!PercolationSample.percolates()){
+                int row = StdRandom.uniform(0,N-1);
+                int col = StdRandom.uniform(0,N-1);
+                if(!PercolationSample.isOpen(row,col)){
+                    PercolationSample.open(row,col);
+                    x=x+1;
+                }
+
+            }
+            ArrayX[i] = x;
+        }
+
+    }   // perform T independent experiments on an N-by-N grid
+    public double mean(){
+        return StdStats.mean(ArrayX);
+    }               // sample mean of percolation threshold
+    public double stddev(){
+        return StdStats.stddev(ArrayX);
+    }                                         // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        return (StdStats.mean(ArrayX)-1.96*Math.sqrt(StdStats.stddev(ArrayX)/TotalTime));
+    }                                 // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return (StdStats.mean(ArrayX)+1.96*Math.sqrt(StdStats.stddev(ArrayX)/TotalTime));
+    }                               // high endpoint of 95% confidence interval
 
 }
